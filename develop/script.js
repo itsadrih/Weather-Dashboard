63909b462819d027651a3e7164054e57f8374964
Adriana Herrera <adri@Adrianas-MacBook-Air.local>
2023-09-25 15:33:19 -0400

    added api and api key, & display weather function.

--- develop/script.js
+++ develop/script.js
@@ -5,4 +5,19 @@ $(document).ready(function () {
       getWeather(city);
   });
 
- 
+  function getWeather(city) {
+    const apiKey = '9e3433936b6e90baa334345a1d72419a'; 
+    
+    
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    
+    
+    $.get(apiUrl, function (data) {
+        displayWeather(data);
+    });
+}
+
+function displayWeather(data) {
+    $('#cityName').text(data.name);
+    $('#temperature').text(`${data.main.temp}°F`);
+    $('#weatherDescription').text(data.weather[0].description);
